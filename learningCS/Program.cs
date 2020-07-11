ab96618e5d482c7173d0c6da067c1abdb9848c4d
silvia <sisa052233@gmail.com>
2020-07-11 20:57:01 +0300

    Add project files.

---
+++ learningCS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace learningCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /___|");
+
+            Console.WriteLine("-------------------");
+
+            string characterName = "Silvia Ashorov";
+            int characterAge = 27;
+            Console.WriteLine("there once was a women named " + characterName + ".");
+            Console.WriteLine("she was " + characterAge + " yeers old and she really liked programming");
+            Console.WriteLine("but she couldn't find a job because of her inexperience");
+            Console.WriteLine("But she still has hope because she knows it's the thing she wants most to do in her life");
+
+
+            Console.ReadLine();
+        }
+    }
+}
